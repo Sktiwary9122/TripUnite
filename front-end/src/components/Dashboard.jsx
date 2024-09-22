65ff831e891809f4a7c6da760f079045dc877fb3
Sktiwary9122 <sktiwary9122@gmail.com>
2024-09-22 18:01:02 +0530

    dashboard

--- front-end/src/components/Dashboard.jsx
+++ front-end/src/components/Dashboard.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Trips from './Trips';
-
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import LanguageSelector from './LanguageSelector';
+import Spinner from './Spinner';
 function Dashboard() {
+  const { t } = useTranslation();
   const [finalData, setFinalData] = useState([]); // initialize as an object
+  const[who,setWho] = useState(true);
   const[userName,setuseName] = useState('');
   
 
@@ -17,6 +22,8 @@ function Dashboard() {
     const userData = JSON.parse(storedLocalData);
     console.log(userData.user.fullName)
     setuseName(userData.user.fullName)
+    setWho(true);
+   
   }
 
   useEffect(() => {
@@ -26,22 +33,54 @@ function Dashboard() {
 
     console.log(userData.user.fullName)
     setuseName(userData.user.fullName)
-  }, [finalData]);
+  }, []);
 
   // Ensure finalData.trips exists and is an array before rendering
   if (!finalData.trips || finalData.trips.length === 0) {
-    return <div>No trips available.</div>; 
+    return <Spinner></Spinner>; 
   }
 
   return (
-    <div className="bg-custom-gradient text-white h-[100vh] flex flex-col">
+    <div className="bg-custom-gradient text-white h-auto flex flex-col">
+    <div className="flex w-full justify-between px-20">
+            <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit' }}>
+              <div className="main-logo"></div>
+            </Link>
+            <div className="flex gap-10 text-white text-xl mt-4">
+              <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit' }}>
+                <ul className="home">{t('home')}</ul>
+              </Link>
+              <ul className="lang">
+                <LanguageSelector />
+              </ul>
+              <Link to="/about" style={{ color: 'inherit', textDecoration: 'inherit' }}>
+                <ul className="about">{t('about')}</ul>
+              </Link>
+              <Link to="/contact" style={{ color: 'inherit', textDecoration: 'inherit' }}>
+                <ul className="main-contact">{t('contactus')}</ul>
+              </Link>
+            </div>
+          </div>
       <h1 className="text-5xl p-10 uppercase">Welcome {userName}</h1>
       <div className="border-t-2 w-[80%] mx-auto flex">
         <div className="border-r-2 w-[20%] p-7 gap-10 flex flex-col">
           <div onClick={fetchuserTrips} className="text-xl p-5">Created trips</div>
           <div className="text-xl p-5 pt-7">Joined trips</div>
         </div>
+        <div>
+        <div>
+        {
+            who ?(
+              <div className="text-xl mx-5 mt-5">
+                  Trips Created by {userName}
+              </div>
+            ):(<div className="text-xl mx-5 mt-5">
+              Trips Joined by {userName}
+            </div>)
+          }
+        </div>
         <div className='flex gap-5 flex-wrap p-5'>
+          
           {finalData.trips.map((trip) => (
             <Trips
               key={trip._id}
@@ -52,6 +91,8 @@ function Dashboard() {
             />
           ))}
         </div>
+        </div>
+        
       </div>
     </div>
   );
